269dd963cd1a24aa04e603472c696d850b7af88a
Alikeyvaz <aeyvazov3@Gmail.com>
2023-02-02 16:45:53 +0400

    Asd

---
+++ ders17/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+namespace ders17
+{
+    internal class Program
+    {
+        public delegate void Chek(string world);
+
+        public static void Main(string[] args)
+        {
+            Test("asd", GetLength);
+            Test( "Asd", GetWord);
+        }
+
+        public static void GetLength(string word)
+        {
+            Console.WriteLine(word.Length);
+
+        }
+
+        public static void GetWord(string word)
+        {
+            Console.WriteLine(word);
+
+        }
+
+        public static void Test(string word, Chek func)
+        {
+            func(word);
+        }
+    }
+}
